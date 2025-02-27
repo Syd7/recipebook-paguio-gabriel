dd9b48f6a34fcd02e4df4d15c8a4b03beaf69d55
Syd <sydpaguio@gmail.com>
2025-02-27 18:25:08 +0800

    Adjusted Views in order to dynamically represent teh data

--- ledger/models.py
+++ ledger/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.urls import reverse
-from datetime import datetime
 
 # Create your models here.
 class Ingredient(models.Model):
@@ -9,7 +8,7 @@ class Ingredient(models.Model):
     def __str__(self):
         return self.name
     def get_absolute_url(self):
-        return reverse('ingredient_detail', args=[str(self.name)])
+        return reverse('ingredient_detail', args=[self.id])
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
@@ -17,9 +16,9 @@ class Recipe(models.Model):
     def __str__(self):
         return self.name
     def get_absolute_url(self):
-        return reverse('recipe_detail', args=[str(self.name)])
+        return reverse('recipe_detail', args=[self.id])
     
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=100)
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="recipe")
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="ingredients")
