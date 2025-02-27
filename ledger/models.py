05ce8a35837d25e3d7a5d5c018797a613766d6a9
Syd <sydpaguio@gmail.com>
2025-02-27 16:20:57 +0800

    Set the String representation and the absolute urls of the Ingredient and Recipe Models

--- ledger/models.py
+++ ledger/models.py
@@ -1,14 +1,25 @@
 from django.db import models
 from django.urls import reverse
+from datetime import datetime
 
 # Create your models here.
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse('ingredient_detail', args=[str(self.name)])
+
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse('recipe_detail', args=[str(self.name)])
     
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=100)
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="ingredients")
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="recipe")
